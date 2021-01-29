62bcafc4c249187f2301ec21f82076aaa70a90af
Rodrigo Santiago <33rodrigosantiago@gmail.com>
2021-01-29 17:30:28 -0300

    Segundo dia Imersão de Dados

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
+import React from 'react';
 import styled from 'styled-components'
 import db from '../db.json';
+import { useRouter } from 'next/router';
+
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
+import Head from 'next/head';
+
 
 const Title = styled.h1`
   font-size: 50px;
@@ -28,15 +34,41 @@ const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  console.log('Retorno', name, setName);
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Quiz - Imersão React 2021</title>
+      </Head>
       <QuizContainer>
+      <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>Quem é esse Pokemon</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Paragrafo teste para visualizar mudanças no Versel</p>
+
+            <form onSubmit={function (event) {
+              event.preventDefault();
+              router.push(`quiz?name=${name}`);
+              console.log('Teste');
+            }}>
+
+              <input onChange={function (evento) { 
+                console.log(evento.target.value);
+                // State
+                // (foto da tela, o que mudou? )
+                setName(evento.target.value);
+              }} 
+              placeholder="Digite seu nome" />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar 
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
@@ -48,7 +80,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/DevSantiro" />
+      <GitHubCorner projectUrl="https://github.com/DevSantiro/santiroquiz" />
     </QuizBackground>
   );
 
---
+++ pages/quiz.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function QuizPage(){
+    return (
+        <div>
+            Página de quiz
+        </div>
+    )
+}
